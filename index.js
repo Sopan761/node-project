c6d99e723dfd216a3b3abeb321a21ae80f3d9f5c
07Sopan <sopan.vinchurkar050@nmims.edu.in>
2025-08-10 13:37:27 +0530

    moved db to atlas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-require('dotenv').config(); // Load .env variables
+//require('dotenv').config(); // Load .env variables
 
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
