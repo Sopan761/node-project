df0a93e76e02ce4e3baaf1559c45f013cc60fa08
Namita18 <namitabarde5@gmail.com>
2025-08-27 18:55:01 +0530

    responsiveness added

--- frontend/frontend1/src/components/Navbar.js
+++ frontend/frontend1/src/components/Navbar.js
@@ -1,74 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="logo-container">
-      <img src={logo} alt="Logo" className="logo-image" />
-      <div className="logo-text">ACOUSΓIC VISIΟNS</div>
-    </div>
-    <ul className="nav-links">
-      <li className="active">
-        <NavLink to="/" style={{ color: "inherit", textDecoration: "none" }}>Home</NavLink>
-      </li>
-      <li><NavLink to="/Aboutus" style={{ color: "inherit", textDecoration: "none" }}>
-         About Us
-      </NavLink></li>
-        <li><NavLink to="/OurProducts" style={{ color: "inherit", textDecoration: "none" }}>
-        Service Catalogue
-      </NavLink>
-      </li>
-      <li className="dropdown">
-        Service Offerings<span className="dropdown-arrow">▼</span>
-        <ul className="dropdown-content">
-          <li>
-            <NavLink to="/services/event-consulting" style={{ color: "inherit", textDecoration: "none" }}>
-              Event Consulting
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/services/audiovisual-Hire" style={{ color: "inherit", textDecoration: "none" }}>
-              Audio-Visual Rental
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/services/eventProduction" style={{ color: "inherit", textDecoration: "none" }}>
-              Event Production Services
-            </NavLink>
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  // Close menu when a link is clicked
+  const handleLinkClick = () => {
+    setIsOpen(false);
+    setDropdownOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo-image" />
+        <div className="logo-text">ACOUSΓIC VISIΟNS</div>
+      </div>
+
+      {/* Hamburger icon */}
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "✖" : "☰"}
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <NavLink to="/" className="nav-link" onClick={handleLinkClick}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Aboutus" className="nav-link" onClick={handleLinkClick}>
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/OurProducts" className="nav-link" onClick={handleLinkClick}>
+            Service Catalogue
+          </NavLink>
+        </li>
+
+        {/* Dropdown */}
+        <li
+          className={`dropdown ${dropdownOpen ? "open" : ""}`}
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+        >
+          Service Offerings <span className={`dropdown-arrow ${dropdownOpen ? "rotate" : ""}`}>▼</span>
+          <ul className={`dropdown-content ${dropdownOpen ? "show" : ""}`}>
+            <li>
+              <NavLink to="/services/event-consulting" className="nav-link" onClick={handleLinkClick}>
+                Event Consulting
+              </NavLink>
             </li>
-          <li>
-            <NavLink to="/services/productionServices" style={{ color: "inherit", textDecoration: "none" }}>
-              Stage And Sound Engineering
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/services/postProductionFilming" style={{ color: "inherit", textDecoration: "none" }}>
-              Production-Ready AV Services
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/services/technicalSupport" style={{ color: "inherit", textDecoration: "none" }}>
-              Technical Support
+            <li>
+              <NavLink to="/services/audiovisual-Hire" className="nav-link" onClick={handleLinkClick}>
+                Audio-Visual Rental
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services/eventProduction" className="nav-link" onClick={handleLinkClick}>
+                Event Production Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services/productionServices" className="nav-link" onClick={handleLinkClick}>
+                Stage And Sound Engineering
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services/postProductionFilming" className="nav-link" onClick={handleLinkClick}>
+                Production-Ready AV Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services/technicalSupport" className="nav-link" onClick={handleLinkClick}>
+                Technical Support
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services/simultaneousInterpretation" className="nav-link" onClick={handleLinkClick}>
+                Complete AV Solutions
+              </NavLink>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <NavLink to="/Contactus" className="nav-link" onClick={handleLinkClick}>
+            Contact Us
           </NavLink>
-          </li>
-          <li>
-          <NavLink to="/services/simultaneousInterpretation" style={{ color: "inherit", textDecoration: "none" }}>
-              Complete AV Solutions, Seamlessly Delivered
-          </NavLink>
-          </li>
-        </ul>
-      </li>
-      <li><NavLink to="/Contactus" style={{ color: "inherit", textDecoration: "none" }}>
-              Contact Us
-          </NavLink></li>
-          {/* <li><NavLink to="/Contactus" style={{ color: "inherit", textDecoration: "none" }}> */}
-              {/* Blog */}
-          {/* </NavLink></li> */}
-    </ul>
-    <input className="search-box" placeholder="search.." />
-  </nav>
-);
+        </li>
+        <li className="search-item">
+          <input className="search-box" placeholder="search.." />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
